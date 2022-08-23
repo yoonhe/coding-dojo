4d7ea44b9cffbc3f10afcff83bd69c702ae38bea
yoonhe <yhehe228@gmail.com>
2022-08-23 09:17:02 +0900

    짝수와 홀수
    정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수

---
+++ EvenAndOdd/java/20220823/src/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+
+        if(num == 0) {
+            return "Even";
+        }
+
+        if(num % 2 == 0) {
+            answer = "Even";
+        }
+
+        if(num % 2 == 1 || num % 2 == -1) {
+            answer = "Odd";
+        }
+
+        return answer;
+    }
+}
