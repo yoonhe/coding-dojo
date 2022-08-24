6006a03e565ab2ee4ebd6db8bb0f9365e09a386e
yoonhe <yhehe228@gmail.com>
2022-08-24 09:42:04 +0900

    Math.min, Math.max 사용해서 중복 제거

--- SumBetweenTwoIntegers/java/20220824/src/Solution.java
+++ SumBetweenTwoIntegers/java/20220824/src/Solution.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Solution {
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
+
+        for(int i = start; i <= end; i += 1) {
+            answer += i;
+        }
+
+        return answer;
+    }
 }
