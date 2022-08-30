58030c9b4c248999d7d99f1062a6cb4cb66ce955
yoonhe <yhehe228@gmail.com>
2022-08-30 09:21:59 +0900

    평균 구하기
    정수를 담고 있는 배열 arr의 평균값을 return하는 함수

---
+++ Average/java/20220830/src/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+
+        int sum = 0;
+
+        for(int number: numbers) {
+            sum += number;
+        }
+
+        return (double) sum / numbers.length;
+    }
+}
